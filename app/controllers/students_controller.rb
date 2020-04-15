bfdc29b1322b0deb14681b79e3dd7411f411e5ce
Kirsten Keels <keelsk@gmail.com>
2020-04-15 12:12:32 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(student_params(:first_name, :last_name))
+    @student = Student.new(student_params)
     @student.save
     redirect_to student_path(@student)
   end
@@ -21,7 +21,7 @@ class StudentsController < ApplicationController
   end
 
   def update
-    @student = Student.update(student_params(:first_name, :last_name))
+    @student = Student.update(student_params
     @student.save
     redirect_to student_path(@student)
   end
