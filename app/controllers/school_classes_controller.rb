1abd2e8149a1a9306ec9e525d1eea6f1b29f7a41
Kirsten Keels <keelsk@gmail.com>
2020-04-15 11:59:47 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -3,15 +3,25 @@ class SchoolClassesController < ApplicationController
     @school_classes = SchoolClass.all
   end
 
-  def new
-  end
-
   def show
     @school_class = SchoolClass.find_by_id(params[:id])
   end
 
+  def new
+  end
+
   def create
     @school_class = SchoolClass.new(post_params(:room_number, :title))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def edit
+    @school_class = SchoolClass.find_by_id(params[:id])
+  end
+
+  def update
+    @school_class = SchoolClass.update(post_params(:room_number, :title))
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
